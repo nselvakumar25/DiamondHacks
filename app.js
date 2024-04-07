7b9a9037a0690abb47a7af223899d574fe0a40d9
stellacse8a <146817134+stellacse8a@users.noreply.github.com>
2024-04-07 15:56:25 -0700

    Update and rename app.py to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Elections from './components/Elections';
+
+function App() {
+  return (
+    <div className="App">
+      <Elections />
+    </div>
+  );
+}
+
+export default App;
